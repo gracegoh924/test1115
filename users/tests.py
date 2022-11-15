4dc9a2eb00044bb418f50e41881b8848c6b279b7
gracegoh <gracegoh924@gmail.com>
2022-11-15 13:20:59 +0900

    Update test.py

--- users/tests.py
+++ users/tests.py
@@ -6,7 +6,7 @@ from users.models import User
 
 class UserRegisteationTest(APITestCase):
     def test_registration(self):
-        url = reverse("user_view")
+        url = reverse("user_view") # 이 이름에 해당하는 url을 가져온다.
         user_data = {
             "username":"gracia",
             "fullname":"graciadivina",
@@ -15,3 +15,5 @@ class UserRegisteationTest(APITestCase):
         }
         response = self.client.post(url, user_data)
         print(response.data)
+        self.assertEqual(response.data["message"], "가입 완료!!")
+        # self.assertEqual(response.data, {"message" : "가입 완료!!"})
